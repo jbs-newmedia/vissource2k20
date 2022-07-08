c913a12399c1dfd3125fea36a470d9c9ed8eeabb
Juergen Schwind <info@jbs-newmedia.de>
2022-07-08 15:04:43 +0200

    Leere Konfiguration angepasst

--- source/vis2.core/stable/frame/engines/vis2.inc.php
+++ source/vis2.core/stable/frame/engines/vis2.inc.php
@@ -16,13 +16,13 @@ $osW_jQuery3=new \osWFrame\Core\jQuery3($osW_Template);
 $osW_jQuery3->load();
 
 $osW_Bootstrap5=new \osWFrame\Core\Bootstrap5($osW_Template);
-if (\osWFrame\Core\Settings::getStringVar('vis2_theme')!==null) {
+if ((\osWFrame\Core\Settings::getStringVar('vis2_theme')!==null)&&(\osWFrame\Core\Settings::getStringVar('vis2_theme')!='')) {
 	$osW_Bootstrap5->setTheme(\osWFrame\Core\Settings::getStringVar('vis2_theme'));
 }
-if (\osWFrame\Core\Settings::getStringVar('vis2_theme_color')!==null) {
+if ((\osWFrame\Core\Settings::getStringVar('vis2_theme_color')!==null)&&(\osWFrame\Core\Settings::getStringVar('vis2_theme_color')!='')) {
 	$osW_Bootstrap5->setCustom('blue', \osWFrame\Core\Settings::getStringVar('vis2_theme_color'));
 }
-if (\osWFrame\Core\Settings::getStringVar('vis2_theme_font')!==null) {
+if ((\osWFrame\Core\Settings::getStringVar('vis2_theme_font')!==null)&&(\osWFrame\Core\Settings::getStringVar('vis2_theme_font')!='')) {
 	$osW_Bootstrap5->setCustom('font-family-sans-serif', \osWFrame\Core\Settings::getStringVar('vis2_theme_font'));
 }
 $osW_Bootstrap5->load();
