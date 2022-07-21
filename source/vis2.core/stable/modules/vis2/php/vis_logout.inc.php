d9747213bd00a3d0e43c9b96ac588323511da7c9
Juergen Schwind <info@jbs-newmedia.de>
2022-07-21 15:31:18 +0200

    Fehler beim ausloggen behoben

--- source/vis2.core/stable/modules/vis2/php/vis_logout.inc.php
+++ source/vis2.core/stable/modules/vis2/php/vis_logout.inc.php
@@ -10,6 +10,6 @@
  * @license MIT License
  */
 
-\osWFrame\Core\Network::directHeader(\osWFrame\Core\Navigation::buildUrl('current', 'vistool='.\osWFrame\Core\Settings::getStringVar('vis2_login_tool')));
+\osWFrame\Core\Network::directHeader(\osWFrame\Core\Navigation::buildUrl('current', 'vistool='.\osWFrame\Core\Settings::getStringVar('vis2_login_module')));
 
 ?>
