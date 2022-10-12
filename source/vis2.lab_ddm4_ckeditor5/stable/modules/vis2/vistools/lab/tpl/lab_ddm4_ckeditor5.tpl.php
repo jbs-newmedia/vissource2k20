a70ee7a5be32df019f849bc04f63a7e31660c172
jschwind <info@juergen-schwind.de>
2022-10-12 21:57:18 +0200

    Seiten hinzugefügt

---
+++ source/vis2.lab_ddm4_ckeditor5/stable/modules/vis2/vistools/lab/tpl/lab_ddm4_ckeditor5.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the VIS2:Lab package
+ *
+ * @author Juergen Schwind
+ * @copyright Copyright (c) JBS New Media GmbH - Juergen Schwind (https://jbs-newmedia.com)
+ * @package VIS2:Lab
+ * @link https://oswframe.com
+ * @license MIT License
+ */
+
+echo $osW_DDM4->runDDMTPL();
+
+?>
