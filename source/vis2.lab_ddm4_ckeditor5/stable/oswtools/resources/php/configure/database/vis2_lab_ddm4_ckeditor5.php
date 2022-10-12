a1d6deef29d0124aba0585eebee4cc52d753798e
jschwind <info@juergen-schwind.de>
2022-10-12 21:56:14 +0200

    Neues Package "VIS2:Lab|DDM4|CKEditor5"

---
+++ source/vis2.lab_ddm4_ckeditor5/stable/oswtools/resources/php/configure/database/vis2_lab_ddm4_ckeditor5.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * This file is part of the VIS2:Lab package
+ *
+ * @author Juergen Schwind
+ * @copyright Copyright (c) JBS New Media GmbH - Juergen Schwind (https://jbs-newmedia.com)
+ * @package VIS2:Lab
+ * @link https://oswframe.com
+ * @license MIT License
+ */
+
+/*
+ * init
+ */
+$__datatable_table='vis2_lab_ddm4_ckeditor5';
+$__datatable_create=false;
+$__datatable_do=false;
+
+/*
+ * check version of table
+ */
+$QreadData=new \osWFrame\Core\Database();
+$QreadData->prepare('SHOW TABLE STATUS LIKE :table:');
+$QreadData->bindString(':table:', $this->getJSONStringValue('database_prefix').$__datatable_table);
+$QreadData->execute();
+if ($QreadData->rowCount()==1) {
+	$QreadData_result=$QreadData->fetch();
+	$avb_tbl=$QreadData_result['Comment'];
+} else {
+	$avb_tbl='0.0';
+}
+$avb_tbl=explode('.', $avb_tbl);
+if (count($avb_tbl)==1) {
+	$av_tbl=intval($avb_tbl[0]);
+	$ab_tbl=0;
+} elseif (count($avb_tbl)==2) {
+	$av_tbl=intval($avb_tbl[0]);
+	$ab_tbl=intval($avb_tbl[1]);
+} else {
+	$av_tbl=0;
+	$ab_tbl=0;
+}
+
+/*
+ * create table
+ */
+if (($av_tbl==0)&&($ab_tbl==0)) {
+	$av_tbl=1;
+	$ab_tbl=0;
+	$__datatable_create=true;
+
+	$QwriteData=new \osWFrame\Core\Database();
+	$QwriteData->prepare('
+CREATE TABLE :table: (
+	ckeditor5_id int(11) unsigned NOT NULL AUTO_INCREMENT,
+	navigation_id int(11) unsigned NOT NULL DEFAULT 0,
+	ckeditor5_example TEXT NOT NULL DEFAULT \'\',
+	ckeditor5_check varchar(16) NOT NULL DEFAULT \'\',
+	element_create_time int(11) unsigned NOT NULL DEFAULT 0,
+	element_create_user_id int(11) unsigned NOT NULL DEFAULT 0,
+	element_update_time int(11) unsigned NOT NULL DEFAULT 0,
+	element_update_user_id int(11) unsigned NOT NULL DEFAULT 0,
+	PRIMARY KEY (ckeditor5_id),
+	KEY navigation_id (navigation_id),
+	KEY element_create_time (element_create_time),
+	KEY element_create_user_id (element_create_user_id),
+	KEY element_update_time (element_update_time),
+	KEY element_update_user_id (element_update_user_id)
+) ENGINE=:engine: DEFAULT CHARSET=:charset: COMMENT=:version:;
+');
+	$QwriteData->bindRaw(':table:', $this->getJSONStringValue('database_prefix').$__datatable_table);
+	$QwriteData->bindString(':engine:', $this->getJSONStringValue('database_engine'));
+	$QwriteData->bindString(':charset:', $this->getJSONStringValue('database_character'));
+	$QwriteData->bindString(':version:', $av_tbl.'.'.$ab_tbl);
+	$QwriteData->execute();
+	if ($QwriteData->hasError()===true) {
+		$tables_error[]='table:'.$__datatable_table.', patch:'.$av_tbl.'.'.$ab_tbl;
+		$db_error[]=$QwriteData->getErrorMessage();
+	}
+}
+
+/*
+ * update table
+ */
+/*
+if (($av_tbl<=1)&&($ab_tbl<1)) {
+	$av_tbl=1;
+	$ab_tbl=1;
+	$__datatable_do=true;
+
+	... code ...
+}
+*/
+
+if ($__datatable_do===true) {
+	$QwriteData=new \osWFrame\Core\Database();
+	$QwriteData->prepare('ALTER TABLE :table: COMMENT=:version:;');
+	$QwriteData->bindRaw(':table:', $this->getJSONStringValue('database_prefix').$__datatable_table);
+	$QwriteData->bindString(':version:', $av_tbl.'.'.$ab_tbl);
+	$QwriteData->execute();
+	if ($QwriteData->hasError()===true) {
+		$tables_error[]='table:'.$__datatable_table.', patch:'.$av_tbl.'.'.$ab_tbl;
+		$db_error[]=$QwriteData->getErrorMessage();
+	}
+}
+
+?>
